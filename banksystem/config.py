079e19e20491fa85002e9cd0be2cd40b3b2a3fe0
BlockMaster <145277345+BlockMaster777@users.noreply.github.com>
2025-11-26 19:38:59 +0300

    Made config.py public

---
+++ banksystem/config.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+# Edit these settings to configure the bank system
+# Do not add sensitive data here, use environment variables instead
+DB_PATH = "./database.db" # From the project root directory
+PORT = 2356
+TOKEN_TTL = 900 # In seconds
+HOST = "localhost"
+ADMIN_UIDS = [1]
