39b2bd7cfa5434a0abce228541352b47daeef72c
BlockMaster <blockmaster350@gmail.com>
2025-11-26 22:21:32 +0300

    Add get id, register_user

--- banksystem/main.py
+++ banksystem/main.py
@@ -50,6 +50,31 @@ def ping():
 def staus():
     return {"message": "ok"}
 
+
+@app.post("/users", responses={413: {"description": "User already exists"},
+                                    500: {"description": "Internal server error"}})
+def register(username: str, password: str):
+    try:
+        user_id = auth_service.register_user_and_get_id(username, password)
+        return {"user_id": user_id}
+    except db_manager.UserAlreadyExistsException:
+        raise HTTPException(status_code=413, detail="User already exists")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+
+
+@app.get("/user_id/{username}", responses={404: {"description": "User dont exist"},
+                                                500: {"description": "Internal server error"}})
+def get_user_id(username: str):
+    try:
+        user_id = auth_service.get_id(username)
+        return {"user_id": user_id}
+    except db_manager.UserDontExistException:
+        raise HTTPException(status_code=404, detail="User dont exist")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal server error {e}")
+
+
 @app.get("/users/{user_id}/balance", responses={400: {"description": "Invalid token"},
                                              404: {"description": "User dont exist"},
                                              500: {"description": "Internal server error"}})
